aa5201ab76cc8f252be215f2d4d3a733d4bc3e42
Ashwin-vemula <ashwin.vemula@toppr.com>
2021-12-06 11:57:17 +0530

    changes

--- scripts/stage-2.15.js
+++ scripts/stage-2.15.js
@@ -411,7 +411,7 @@ $(".subject-card-sp").click((e) => {
 const isExist = () => {
   $.ajax({
     type: "POST",
-    url: `${PROD_BASE_URL}/api/V1/userDetail/existByEmailOrMobile?timezone=${timeZone}&regionId=${country}&courseType=ALL&brandId=byju&timestamp=${new Date().getTime()}`,
+    url: `${PROD_BASE_URL}/api/V1/userDetail/existByEmailOrMobile?timezone=${timeZone}&regionId=${country}&courseType=MATH&brandId=byju&timestamp=${new Date().getTime()}`,
     cache: false,
     data: { mobile: parentMobileNum, dialCode },
 
@@ -615,8 +615,8 @@ const otpTimer = () => {
 
 const getOtp = (callback, isResend) => {
   const url = isUserExist
-    ? `${PROD_BASE_URL}/api/V1/users/sendStudentVerificationCode?timezone=${timeZone}&regionId=${country}&courseType=ALL&brandId=byju&timestamp=${new Date().getTime()}`
-    : `${PROD_BASE_URL}/api/V1/otp/generate?regionId=${country}&timezone=${timeZone}&courseType=ALL&brandId=byju&timestamp=${new Date().getTime()}`;
+    ? `${PROD_BASE_URL}/api/V1/users/sendStudentVerificationCode?timezone=${timeZone}&regionId=${country}&courseType=MATH&brandId=byju&timestamp=${new Date().getTime()}`
+    : `${PROD_BASE_URL}/api/V1/otp/generate?regionId=${country}&timezone=${timeZone}&courseType=MATH&brandId=byju&timestamp=${new Date().getTime()}`;
 
   $.ajax({
     type: "POST",
@@ -671,8 +671,8 @@ $(".otp-input-box").on("input", (e) => {
     window.WHJR_ANALYTICS.trackEvent("Verification code entered", {});
 
     const url = isUserExist
-      ? `${PROD_BASE_URL}/api/V1/users/authenticateVerificationCode?timezone=${timeZone}&_vercel_no_cache=1&regionId=${country}&courseType=ALL&brandId=byju&timestamp=${new Date().getTime()}`
-      : `${PROD_BASE_URL}/api/V1/otp/verify?timezone=${timeZone}&regionId=${country}&brandId=byju&courseType=ALL&timestamp=${new Date().getTime()}`;
+      ? `${PROD_BASE_URL}/api/V1/users/authenticateVerificationCode?timezone=${timeZone}&_vercel_no_cache=1&regionId=${country}&courseType=MATH&brandId=byju&timestamp=${new Date().getTime()}`
+      : `${PROD_BASE_URL}/api/V1/otp/verify?timezone=${timeZone}&regionId=${country}&brandId=byju&courseType=MATH&timestamp=${new Date().getTime()}`;
     $.ajax({
       type: "POST",
       url: url,
@@ -742,7 +742,7 @@ const handleUserPropsAnalytics = (student) => {
 const handleRegisterUser = () => {
   $.ajax({
     type: "POST",
-    url: `${PROD_BASE_URL}/api/V1/trial/users/minimalFieldRegister?timezone=${timeZone}&courseType=ALL&isMobilePlatform=false&brandId=byju&timestamp=${new Date().getTime()}`,
+    url: `${PROD_BASE_URL}/api/V1/trial/users/minimalFieldRegister?timezone=${timeZone}&courseType=MATH&isMobilePlatform=false&brandId=byju&timestamp=${new Date().getTime()}`,
     cache: false,
     data: {
       mobile: parentMobileNum,
@@ -778,7 +778,7 @@ const handleRegisterUser = () => {
 const handleMecall = () => {
   $.ajax({
     type: "GET",
-    url: `${PROD_BASE_URL}/api/V1/userDetail/me?timezone=${timeZone}&courseType=ALL&brandId=byju&timestamp=${new Date().getTime()}`,
+    url: `${PROD_BASE_URL}/api/V1/userDetail/me?timezone=${timeZone}&courseType=MATH&brandId=byju&timestamp=${new Date().getTime()}`,
     cache: false,
     headers: {
       authorization: `Bearer ${token}`,
